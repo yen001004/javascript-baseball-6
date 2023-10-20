c8a5efb69d5715eccd6c7355f3e984f37e39a2a6
yen001004 <yenn001004@gmail.com>
2023-10-20 22:23:00 +0900

    feat: userNum 리턴값 변경

--- src/functions/getUserNum.js
+++ src/functions/getUserNum.js
@@ -1,17 +1,10 @@
 import { Console } from "@woowacourse/mission-utils";
-import {
-  REPLAY_CODE,
-  EXIT_CODE,
-  MESSAGE,
-  ERROR_MESSAGE,
-} from "../constants/constants.js";
+import { MESSAGE, ERROR_MESSAGE } from "../constants/constants.js";
 
 async function getUserNum() {
   try {
     const num = await Console.readLineAsync(MESSAGE.PROMPT);
-    if (num == REPLAY_CODE || num == EXIT_CODE) return true;
-    Console.print(num);
-    return false;
+    return [...num].map(Number);
   } catch (error) {
     Console.print(ERROR_MESSAGE.ERROR);
   }
