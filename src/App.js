c64ba8b2110d8b01fd98e4bb18cae95304c7da5f
yen001004 <yenn001004@gmail.com>
2023-10-20 17:15:20 +0900

    feat: Console 입출력 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-class App {
-  async play() {}
+import { Console } from "@woowacourse/mission-utils";
+import {
+  ERROR_MESSAGE,
+  EXIT_CODE,
+  MESSAGE,
+  REPLAY_CODE,
+} from "./constants/constants";
+
+async function getNumber() {
+  try {
+    const num = await Console.readLineAsync(MESSAGE.PROMPT);
+    if (num == REPLAY_CODE || num == EXIT_CODE) return true;
+    Console.print(num);
+    return false;
+  } catch (error) {
+    Console.print(ERROR_MESSAGE.ERROR);
+  }
 }
+
+class App {
+  async play() {
+    Console.print(MESSAGE.START);
+
+    let isFinished = false;
+    while (!isFinished) {
+      isFinished = await getNumber();
+    }
+  }
+}
+
+const app = new App();
+app.play();
 
 export default App;
---
+++ src/constants/constants.js
@@ -0,0 +1,16 @@
+const REPLAY_CODE = 1;
+const EXIT_CODE = 2;
+
+const MESSAGE = Object.freeze({
+  START: "숫자 야구 게임을 시작합니다.",
+  PROMPT: "숫자를 입력해주세요 : ",
+  SOLVED: "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+  REPLAY_OR_EXIT: "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.",
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  ERROR: "[ERROR]",
+  NUM_FORMAT_ERROR: "[ERROR] 숫자가 잘못된 형식입니다.",
+});
+
+export { REPLAY_CODE, EXIT_CODE, MESSAGE, ERROR_MESSAGE };
