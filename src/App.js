0fe8a9946ec41092fcc48f1f836dd47ddb1ad6b0
yen001004 <yenn001004@gmail.com>
2023-10-21 23:05:49 +0900

    refactor: startGame 함수 App의 메소드로 변경

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
 import { Console } from "@woowacourse/mission-utils";
-import { ERROR_MESSAGE, MESSAGE } from "./constants/constants.js";
+import { MESSAGE } from "./constants/constants.js";
 import getUserNum from "./functions/getUserNum.js";
 import generateNum from "./functions/generateNum.js";
 import getExitCode from "./functions/getExitCode.js";
 import getScore from "./functions/getScore.js";
 import printScore from "./functions/printScore.js";
 
-async function playGame() {
-  const computerNum = generateNum();
-  // Console.print(computerNum);
+class App {
+  async startGame() {
+    const computerNum = generateNum();
 
-  while (true) {
-    const userNum = await getUserNum();
+    while (true) {
+      const userNum = await getUserNum();
 
-    const score = getScore(computerNum, userNum);
-    printScore(score);
+      const score = getScore(computerNum, userNum);
+      printScore(score);
 
-    if (score.strike === 3) {
-      Console.print(MESSAGE.SOLVED);
-      try {
-        const exit = await getExitCode();
-        return exit;
-      } catch (error) {
-        Console.print(error);
-        return true;
+      if (score.strike === 3) {
+        Console.print(MESSAGE.SOLVED);
+        try {
+          const exit = await getExitCode();
+          return exit;
+        } catch (error) {
+          Console.print(error);
+          return true;
+        }
       }
     }
   }
-}
 
-class App {
   async play() {
     Console.print(MESSAGE.START);
 
     let isFinished = false;
     while (!isFinished) {
-      isFinished = await playGame();
+      isFinished = await this.startGame();
     }
   }
 }
