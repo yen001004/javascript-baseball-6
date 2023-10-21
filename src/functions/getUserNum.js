f265587b85732e8e0a47ac7b93578e69fadec274
yen001004 <yenn001004@gmail.com>
2023-10-21 23:23:54 +0900

    refactor: 유효성 검사 매직 넘버 수정

--- src/functions/getUserNum.js
+++ src/functions/getUserNum.js
@@ -1,5 +1,5 @@
 import { Console } from "@woowacourse/mission-utils";
-import { MESSAGE, ERROR_MESSAGE } from "../constants/constants.js";
+import { MESSAGE, ERROR_MESSAGE, NUM_LENGTH } from "../constants/constants.js";
 
 async function getUserNum() {
   const num = await Console.readLineAsync(MESSAGE.PROMPT);
@@ -8,7 +8,7 @@ async function getUserNum() {
     throw new Error(ERROR_MESSAGE.NUM_FORMAT_ERROR);
   }
   const uniqueNum = new Set(num);
-  if (uniqueNum.size !== 3) {
+  if (uniqueNum.size !== NUM_LENGTH) {
     throw new Error(ERROR_MESSAGE.DUPLICATE_NUM_ERROR);
   }
 
