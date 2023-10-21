6b3646e6d46294414bcf9acba49cb8068a88fc00
yen001004 <yenn001004@gmail.com>
2023-10-21 23:08:49 +0900

    refactor: generateNum 공백 추가

--- src/functions/generateNum.js
+++ src/functions/generateNum.js
@@ -3,8 +3,10 @@ import { MAX_NUM, MIN_NUM, NUM_LENGTH } from "../constants/constants.js";
 
 function generateNum() {
   const computer = [];
+
   while (computer.length < NUM_LENGTH) {
     const number = MissionUtils.Random.pickNumberInRange(MIN_NUM, MAX_NUM);
+
     if (!computer.includes(number)) {
       computer.push(number);
     }
